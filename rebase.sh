dfdaa31880809d0e4a102c74547f300c0269d84a
Ole Rohne <o.m.rohne@fys.uio.no>
2018-12-29 17:19:31 +0100

    On branch 0/local/0/git - fixed rebase.sh and mirror.sh Changes to be committed: 	new file:   mirror.sh 	modified:   rebase.sh

--- rebase.sh
+++ rebase.sh
@@ -1,10 +1,9 @@
 git stash
 git fetch origin
 git checkout -B master origin/master
-git branch | grep / | sort | while read b; do echo Rebasing $b; git rebase master $b; done
+git branch | grep / | sort | while read b; do echo Rebasing $b; git rebase master $b || break; done
 git branch -D merged.old
 git branch -M merged merged.old
 git checkout -B merged master
-git branch | grep / | sort | while read b; do echo Merging $b; git merge $b; done
-git push --mirror mirror
+git branch | grep / | sort | while read b; do echo Merging $b; git merge $b || break; done
 git stash pop
